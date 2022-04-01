0ad56f67df90003b04045847b928186b0f490dbb
Mhmd <biboo.biboo97@gmail.com>
2022-04-01 05:24:07 +0200

    Remove unnecessary code

--- src/index.js
+++ src/index.js
@@ -15,8 +15,3 @@ app.render(
 );
 
 serviceWorkerRegistration.register();
-/*if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("service-worker.js").then(function () {
-    console.log("Service Worker Registered");
-  });
-}*/
